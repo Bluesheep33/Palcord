b5bdae8af87e02954e9266d93a43cd8cf093d257
Lukas Mouthaan <L.A.mouthaan@student.tudelft.nl>
2024-05-31 17:55:36 +0200

    create script to log in as bot in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 require('dotenv').config();
+console.log(process.env.TOKEN);
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN).then(() => {});
