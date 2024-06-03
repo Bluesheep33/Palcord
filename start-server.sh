381df456016d9413faf4e8f235928c7939f1e812
Lukas Mouthaan <L.A.mouthaan@student.tudelft.nl>
2024-06-03 13:22:45 +0200

    Create update-bot script

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#run using 'bash start-server.sh' or run 'chmod +x start-server.sh' once and then './start-server.sh'
 ~/Steam/steamapps/common/PalServer/PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS 2>&1 | tee -a ~/Steam/steamapps/common/PalServer/console-log.txt
---
+++ update-bot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#run using 'bash update-bot.sh' or run 'chmod +x update-bot.sh' once and then './update-bot.sh'
+git pull origin main
