ff27cf93917e7578b83639d71ce32b2e2e11265f
Lukas Mouthaan <L.A.mouthaan@student.tudelft.nl>
2024-06-03 17:49:58 +0200

    Parse stdout json

--- src/handlers/stdoutLineHandler.js
+++ src/handlers/stdoutLineHandler.js
@@ -1,3 +1,17 @@
 module.exports = (line) => {
-    console.log(line);
+    try {
+        // Try to parse the line as JSON
+        const obj = JSON.parse(line);
+
+        // Log the object
+        console.log(obj);
+
+        // Log each attribute of the object
+        for (const key in obj) {
+            console.log(`${key}: ${obj[key]}`);
+        }
+    } catch (error) {
+        // If an error is thrown, the line is not JSON, so just log the line
+        console.log(line);
+    }
 }
