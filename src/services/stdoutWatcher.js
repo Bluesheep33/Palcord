8b693a81d361eb064a64642552886a014e8b35a3
Lukas Mouthaan <L.A.mouthaan@student.tudelft.nl>
2024-06-03 13:08:19 +0200

    Send only new stdout lines to handler

--- src/services/stdoutWatcher.js
+++ src/services/stdoutWatcher.js
@@ -4,36 +4,27 @@ const stdoutHandler = require('../handlers/stdoutHandler');
 const { logPath } = require('../../config.json');
 
 module.exports = () => {
-    let lineCount = 0;
-
-    const rl = readline.createInterface({
-        input: fs.createReadStream(logPath),
-        output: process.stdout,
-        terminal: false
-    });
-
-    rl.on('line', () => {
-        lineCount++;
-    });
+    let lastLineRead = 0;
 
     fs.watchFile(logPath, (curr, prev) => {
         if (curr.mtime !== prev.mtime) {
-            const newRl = readline.createInterface({
+            let lineCount = 0;
+
+            const rl = readline.createInterface({
                 input: fs.createReadStream(logPath),
-                output: process.stdout,
                 terminal: false
             });
 
-            let newLines = 0;
-
-            newRl.on('line', (line) => {
-                newLines++;
-                if (newLines > lineCount) {
+            rl.on('line', (line) => {
+                lineCount++;
+                if (lineCount > lastLineRead) {
                     stdoutHandler(line);
                 }
             });
 
-            lineCount = newLines;
+            rl.on('close', () => {
+                lastLineRead = lineCount;
+            });
         }
     });
 }
